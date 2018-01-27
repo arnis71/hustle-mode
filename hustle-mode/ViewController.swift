85cf7409051bf4f30e3d0bf54c44bb2f1e205270
Arnis Shaykh <arnis71@gmail.com>
2018-01-27 17:35:21 +0300

    first ios project

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var holder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustle: UILabel!
+    @IBOutlet weak var on: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("kjhsdkfjh")
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonClick(_ sender: Any) {
+        holder.isHidden = false
+        background.isHidden = true
+        button.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 200, width: 375, height: 267)
+        }) { (finished) in
+            self.hustle.isHidden = false
+            self.on.isHidden = false
+        }
     }
-
-
 }
 
